f18654ba6067a1233d07c33099d3b7985325d02f
zjun <565061763@qq.com>
2025-10-18 00:30:04 +0800

    支持跳过执行

--- lib/src/main/java/com/sd/lib/rty/Rty.kt
+++ lib/src/main/java/com/sd/lib/rty/Rty.kt
@@ -19,29 +19,25 @@ suspend fun <T> rty(
   /** 失败回调，返回false停止执行 */
   onFailure: RtyScope.(Throwable) -> Boolean = { true },
   /** 执行回调 */
-  block: suspend RtyScope.() -> T,
+  block: suspend RtyDoingScope.() -> T,
 ): Result<T> {
   require(maxCount > 0)
   with(RtyScopeImpl()) {
     while (true) {
-      increaseCount()
-
-      val result = runCatching {
-        block()
-      }.onFailure { e ->
-        if (e is CancellationException) throw e
-      }
+      val result = runCatching { block() }
+        .onFailure { e -> if (e is CancellationException) throw e }
 
       currentCoroutineContext().ensureActive()
-      if (result.isSuccess) {
-        return result
-      }
+      if (result.isSuccess) return result
 
       val exception = checkNotNull(result.exceptionOrNull())
-      val shouldContinue = onFailure(exception).also { currentCoroutineContext().ensureActive() }
-      if (!shouldContinue) {
-        return result
+      if (exception is SkipRtyException) {
+        delay(getDelay())
+        continue
       }
+
+      val shouldContinue = onFailure(exception).also { currentCoroutineContext().ensureActive() }
+      if (!shouldContinue) return result
 
       if (rtyCount >= maxCount) {
         // 达到最大执行次数
@@ -49,6 +45,7 @@ suspend fun <T> rty(
       } else {
         // 延迟后继续执行
         delay(getDelay())
+        increaseCount()
         continue
       }
     }
@@ -60,8 +57,17 @@ interface RtyScope {
   val rtyCount: Int
 }
 
-private class RtyScopeImpl : RtyScope {
-  private var _count = 0
+interface RtyDoingScope : RtyScope {
+  /** 跳过本次执行，本次不计数 */
+  fun skip(): Nothing
+}
+
+private class RtyScopeImpl : RtyDoingScope {
+  private var _count = 1
+
   override val rtyCount: Int get() = _count
+  override fun skip(): Nothing = throw SkipRtyException()
   fun increaseCount() = _count++
 }
+
+private class SkipRtyException : Exception()
